ff5d214445e4dd7f8ad10361dd719c1840f29c20
mhmd manfy <mohammedmanfy29@gmail.com>
2022-10-12 23:43:23 +0300

    add navbar and footer and slider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './CommonComponents/navbar/navbar.component';
+import { FooterComponent } from './CommonComponents/footer/footer.component';
+import { HomeComponent } from './HomeComponent/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
